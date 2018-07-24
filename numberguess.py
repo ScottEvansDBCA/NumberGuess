d349cbc64d6044811510a63ef608e5f66b291417
scottevansdbca <scott.evans@dbca.wa.gov.au>
2018-07-24 16:05:00 +0800

    Added replay to game

--- numberguess.py
+++ numberguess.py
@@ -10,17 +10,26 @@ def generate_number():
 
 secret_number = generate_number()
 
+def main():
+    winning_number = secret_number
+    print("The game has a picked a secret number between 0 and 10.\nCan you guess what it is?")
+    while winning_number == secret_number:
+        guess = input("Please enter your guess: ")
+        higher_or_lower(guess)
+
 def higher_or_lower(guess):
     global secret_number
     if guess < secret_number:
-       print('Higher')
+        print('Higher')
     elif guess > secret_number:
-       print('Lower')
+        print('Lower')
     else:
-       print('Congratulations, you win!')
-       secret_number = generate_number()
+        print('Congratulations, you win!')
+        secret_number = generate_number()
+        play_again = raw_input("Would you like to play again? Y/N: ")
+        if play_again.upper() == 'Y':
+           main()
+        else:
+            pass
 
-winning_number = secret_number
-while winning_number == secret_number:
-    guess = input("Please enter your guess: ")
-    higher_or_lower(guess)
+main()
